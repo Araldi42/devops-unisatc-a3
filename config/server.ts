9422b0185700bb88cd78905fb08c2feaf883efca
angelo.rosa <angelo.rosa@pulsati.com.br>
2025-11-27 13:00:10 -0300

    Adiciona testes E2E com playwright

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,12 @@
 export default ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
+  host: env('HOST', '127.0.0.1'),
   port: env.int('PORT', 1337),
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array('APP_KEYS') || [
+      'ImJMpHjnCdJw4ii7jZzCXQ==',
+      'Jg239VoMach6Fh2LAH6ydA==',
+      'LAdmPTwE8oqyVjAV4pCkBQ==',
+      'f1gPGngKmE5xhyDktSpCVw==',
+    ],
   },
 });
---
+++ playwright.config.ts
@@ -0,0 +1,24 @@
+import { PlaywrightTestConfig } from '@playwright/test';
+
+const config: PlaywrightTestConfig = {
+  testDir: 'src/scenarios',
+  timeout: 50000,
+  retries: 0,
+  webServer: {
+    command: 'pnpm run develop',
+    url: 'http://localhost:1337',
+    timeout: 50000,
+    reuseExistingServer: !process.env.CI,
+  },
+  use: {
+    trace: 'on',
+    locale: 'pt-BR',
+    headless: false,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    screenshot: 'on',
+    video: 'off'
+  },
+  reporter: [['html', { outputFolder: 'artifacts/report', open: 'never' }]]
+};
+export default config;
